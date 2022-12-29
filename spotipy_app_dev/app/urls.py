c4d7728d2ff74732ca24505229ecf82778456e70
Zento <blborismichael@gmail.com>
2022-12-29 15:29:58 +0200

    idk

--- spotipy_app_dev/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from spotipy_app_dev.app.views import Index, Info
-
-urlpatterns = [
-    path('', Index.as_view(), name='index'),
-    path('info/', Info.as_view(), name='info'),
-]
--- spotipy_app_dev/spotipy/base/getters.py
+++
@@ -1,5 +0,0 @@
-from spotipy_app_dev.spotipy.base.classes import Track
-
-
-def get_track_object(track_id):
-    return Track(track_id)
